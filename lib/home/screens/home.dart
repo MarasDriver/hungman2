1625c046feca690e20cff01637c74b32562ef21e
MarasDriver <mkamzel@hotmail.com>
2022-02-19 14:50:32 +0100

    image

--- lib/home/screens/home.dart
+++ lib/home/screens/home.dart
@@ -17,9 +17,11 @@ class Home extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Image(image: AssetImage("assets/img/a.jpg")),
+            Image.asset("assets/img/a.jpg"),
             ElevatedButton(onPressed: () {}, child: Text("New Game")),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 75.0),
+              padding: const EdgeInsets.symmetric(vertical: 70.0),
               child: ElevatedButton(
                   onPressed: () {}, child: Text("Wall of the hanged")),
             ),
